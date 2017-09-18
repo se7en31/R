bcec643a6302f6f3a89e64c4fb5f3b274c7b9e0c
se7en31 <32049165+se7en31@users.noreply.github.com>
2017-09-18 16:55:02 -0700

    Update RWL2CSV.R

--- RWL2CSV.R
+++ RWL2CSV.R
@@ -1,5 +1,5 @@
 library(dplR)
-#setwd("C:/Users/Twitch/Downloads/R-Stuff-From-Aaron")
+setwd("C:/Users/Twitch/Downloads/R-Stuff-From-Aaron")
 fileNames <- Sys.glob("*.rwl")
 for (fileName in fileNames) {
 	tempName<-unlist(strsplit(fileName, split='.', fixed=TRUE))[1]
