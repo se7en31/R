ebb2315200a9f5e54ccdf9c2afe836d2683b2cff
se7en31 <32049165+se7en31@users.noreply.github.com>
2017-09-17 19:46:03 -0700

    Add files via upload

---
+++ RWL2CSV.R
@@ -0,0 +1,10 @@
+library(dplR)
+setwd("C:/Users/Twitch/Downloads/R-Stuff-From-Aaron")
+fileNames <- Sys.glob("*.rwl")
+for (fileName in fileNames) {
+	tempName<-unlist(strsplit(fileName, split='.', fixed=TRUE))[1]
+	tempRwl<-read.rwl(fileName)
+	tempBasal<-bai.out(tempRwl,diam=NULL)
+	tempCsv<-paste(tempName, "csv", sep=".")
+	write.csv(tempBasal,file=tempCsv)
+}
